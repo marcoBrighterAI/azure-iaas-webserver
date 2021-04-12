fb415c26fe63499503ee94f16025296d74c6b10e
marcoBrighterAI <marco@brighter.ai>
2021-04-12 11:36:42 +0200

    fix: address_prefixes wrong value

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_subnet" "internal" {
   name                 = "${var.prefix}-internal-subnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/16"]
+  address_prefixes     = ["10.0.0.0/16"]
 }
 
 resource "azurerm_network_interface" "main" {
