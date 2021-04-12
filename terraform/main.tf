5e6447aeb84b9061a46aa186d476ba38ec4875fd
Marco Paspuel <58422809+marcoBrighterAI@users.noreply.github.com>
2021-04-12 14:25:40 +0200

    feat: Add terraform template (#3)
    * feat: fist draft of terraform template
    
    * feat: add infrastructure requirements to terraform template
    
    * style: change prefix, tag name and resource group name
    
    * fix: address_prefixes wrong value
    
    * fix: change not compliant password from vm
    
    * chore: add terraform to git ignore
    
    * chore: add solution.plan
    
    * chore: remove subscription id from variables.tf

---
+++ terraform/main.tf
@@ -0,0 +1,202 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+
+  tags = {
+    udacity = var.prefix
+  }
+}
+
+// Main virtual network, subnets, nics and public IPs
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tags = {
+    udacity = var.prefix
+  }
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "${var.prefix}-internal-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.0.0/16"]
+}
+
+resource "azurerm_network_interface" "main" {
+  count               = var.instance_count
+  name                = "${var.prefix}-nic-${count.index}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "${var.prefix}-primary"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    udacity = var.prefix
+  }
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.prefix}-public-ip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    udacity = var.prefix
+  }
+}
+
+// Security Group
+resource "azurerm_network_security_group" "webserver" {
+  name                = "${var.prefix}-webserver-sg"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tags = {
+    udacity = var.prefix
+  }
+}
+
+resource "azurerm_network_security_rule" "internal-inbound" {
+  name                        = "internal-inbound-rule"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.webserver.name
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "10.0.0.0/16"
+  destination_address_prefix  = "10.0.0.0/16"
+}
+
+resource "azurerm_network_security_rule" "internal-outbound" {
+  name                        = "internal-outbound-rule"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.webserver.name
+  priority                    = 102
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "10.0.0.0/16"
+  destination_address_prefix  = "10.0.0.0/16"
+}
+
+resource "azurerm_network_security_rule" "external" {
+  name                        = "external-rule"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.webserver.name
+  priority                    = 103
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
+
+// Load balancer
+
+resource "azurerm_lb" "main" {
+  name                = "${var.prefix}-lb"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  frontend_ip_configuration {
+    name                 = "${var.prefix}-public-address"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+
+  tags = {
+    udacity = var.prefix
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  resource_group_name = azurerm_resource_group.main.name
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "${var.prefix}-backend-address-pool"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "main" {
+  count                   = var.instance_count
+  backend_address_pool_id = azurerm_lb_backend_address_pool.main.id
+  ip_configuration_name   = "${var.prefix}-primary"
+  network_interface_id    = element(azurerm_network_interface.main.*.id, count.index)
+}
+
+resource "azurerm_availability_set" "avset" {
+  name                = "${var.prefix}-avset"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tags = {
+    udacity = var.prefix
+  }
+}
+
+
+resource "azurerm_linux_virtual_machine" "main" {
+  count                           = var.instance_count
+  name                            = "${var.prefix}-vm-${count.index}"
+  resource_group_name             = azurerm_resource_group.main.name
+  source_image_id                 = var.image_id
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_D2s_v4"
+  admin_username                  = var.username
+  admin_password                  = var.password
+  availability_set_id             = azurerm_availability_set.avset.id
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.main[count.index].id,
+  ]
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  tags = {
+    udacity = var.prefix
+  }
+}
+
+resource "azurerm_managed_disk" "data" {
+  count                = var.instance_count
+  name                 = "${var.prefix}-disk-${count.index}"
+  location             = azurerm_resource_group.main.location
+  create_option        = "Empty"
+  disk_size_gb         = 10
+  resource_group_name  = azurerm_resource_group.main.name
+  storage_account_type = "Standard_LRS"
+
+  tags = {
+    udacity = var.prefix
+  }
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "data" {
+  virtual_machine_id = element(azurerm_linux_virtual_machine.main.*.id, count.index)
+  managed_disk_id    = element(azurerm_managed_disk.data.*.id, count.index)
+  lun                = 1
+  caching            = "None"
+  count              = var.instance_count
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default     = "udacity-project1"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default     = "West Europe"
+}
+
+variable "username" {
+  description = "The User Name for the virtual machine"
+  default     = "developer"
+}
+
+variable "password" {
+  description = "The password for the virtual machine"
+  default     = "Developer1234"
+}
+
+variable "image_id" {
+  description = "Enter the ID for the image which will be used for creating the Virtual Machines"
+  default     = "/subscriptions/SUBSCRIPTION_ID/resourceGroups/RESOURCE_GROUP_NAME/providers/Microsoft.Compute/images/IMAGE_NAME"
+}
+
+variable "instance_count" {
+  description = "Enter number of vm instances to create"
+}
