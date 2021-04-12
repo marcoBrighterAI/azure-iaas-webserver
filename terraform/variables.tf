3c41c542336f68c3bc1df01e2b0d6e5ebb52efdd
marcoBrighterAI <marco@brighter.ai>
2021-04-12 10:22:39 +0200

    feat: fist draft of terraform template

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "username" {
+  description = "The User Name for the virtual machine"
+}
+
+variable "password" {
+  description = "The password for the virtual machine"
+}
